3edad7a12dad780adeac9fccf00cb3049a906581
Ryuji TSUTSUI <ryu22e@gmail.com>
2023-01-16 20:10:13 +0900

    Add django-prometheus configuration

--- django_prometheus_example/settings.py
+++ django_prometheus_example/settings.py
@@ -37,15 +37,18 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_prometheus",
 ]
 
 MIDDLEWARE = [
+    "django_prometheus.middleware.PrometheusBeforeMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
+    "django_prometheus.middleware.PrometheusAfterMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
